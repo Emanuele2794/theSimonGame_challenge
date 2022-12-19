fa2363fc52d0b7695a9baa35bd08bb5201315a56
Emanuele Rappa <96550342+Emanuele2794@users.noreply.github.com>
2022-12-19 21:07:06 +0100

    aggiornato 19/12
    Aggiornato con le indicazioni di Angela Yu

--- game.js
+++ game.js
@@ -2,20 +2,15 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
-
 var userClickedPattern = [];
-
 
 $(".btn").click(function() {
 
- 
   var userChosenColour = $(this).attr("id");
-
-  
   userClickedPattern.push(userChosenColour);
 
-  //console.log(userClickedPattern);
+ 
+  playSound(userChosenColour);
 
 });
 
@@ -27,6 +22,14 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  
+  playSound(randomChosenColour);
+}
+
+
+function playSound(name) {
+
+
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
